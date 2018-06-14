f493f310e721e1d05e4f1731f28d020a27f34348
FengChueng <yinglongChueng@gmail.com>
2018-06-14 19:01:13 +0800

    fix

--- src/main/java/com/zyl/sercurity/service/UserService.java
+++ src/main/java/com/zyl/sercurity/service/UserService.java
@@ -3,6 +3,12 @@ package com.zyl.sercurity.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,6 +17,11 @@ import com.zyl.sercurity.pojo.User;
 
 @Service
 public class UserService {
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    
+    
     private static final Map<String, User> userlist = new HashMap<>();
     static {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
@@ -32,5 +43,27 @@ public class UserService {
         User user = userlist.put(username, new User(username, encoder.encode(password), "ROLE_USER"));
         return user;
     }
+    
+//    public String login(String username, String password) {  
+//        UsernamePasswordAuthenticationToken upToken = new UsernamePasswordAuthenticationToken(username, password);  
+//        // Perform the security  
+//        final Authentication authentication = authenticationManager.authenticate(upToken);  
+//        SecurityContextHolder.getContext().setAuthentication(authentication);  
+//        // Reload password post-security so we can generate token  
+//        final UserDetails userDetails = userDetailsService.loadUserByUsername(username);  
+//        final String token = jwtTokenUtil.generateToken(userDetails);  
+//        return token;  
+//    }  
+//  
+//    public String refresh(String oldToken) {  
+//        final String token = oldToken.substring(tokenHead.length());  
+//        String username = jwtTokenUtil.getUsernameFromToken(token);  
+//        SysUser user = (SysUser) userDetailsService.loadUserByUsername(username);  
+//        if (jwtTokenUtil.canTokenBeRefreshed(token, user.getLastPasswordResetDate())){  
+//            return jwtTokenUtil.refreshToken(token);  
+//        }  
+//        return null;  
+//    }  
+    
     
 }
